a0027067c6cb670065c15e1557b4585c37bf9eaf
Shikharsingh Kshatriya <shikharsingh25786@gmail.com>
2024-07-05 02:30:10 +0530

    Added Particle script on gameobject destroy

--- Assets/Scripts/DoorOne.cs
+++ Assets/Scripts/DoorOne.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoorOne : MonoBehaviour
 {
     [SerializeField] private List<EnemySpawner> enemySpawners;
-
+  
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("KeyOne"))
         {
-            if(AudioManager.instance != null)
+            
+            
+            if (AudioManager.instance != null)
             {
                 AudioManager.instance.PlayDoorOpenSound();
             }
-            Destroy(gameObject);
+            
+            GetComponent<ParticleSystem>().Play();
+            GetComponent<SpriteRenderer>().enabled = false;
             Destroy(other.gameObject);
+            Invoke(nameof(DestroyObj), 0.1f);
             foreach (EnemySpawner spawner in enemySpawners)
             {
                 if (spawner != null)
@@ -24,6 +31,10 @@ public class DoorOne : MonoBehaviour
                 }
             }
         }
+    }
+    private void DestroyObj()
+    {
+        Destroy(gameObject);
     }
 }
 
