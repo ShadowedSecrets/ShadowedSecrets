20f4a5c24b16b3475024998b32191ed5f515458b
InfectedCosta <JustinCosta4242@gmail.com>
2024-07-04 20:39:35 -0400

    Echo Room Complete
    -Added pressure plates, and false door system for puzzles.

---
+++ Assets/PressurePlate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public bool isActivated = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Pushable"))
+        {
+            isActivated = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Pushable"))
+        {
+            isActivated = false;
+        }
+    }
+}
+
