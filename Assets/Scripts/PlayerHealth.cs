bee1baf67e57c8361087f0c81a514bf3bdb65b83
muhammetakkoc <akkoc.muhammet0@gmail.com>
2024-07-12 01:56:00 -0400

    CheckPoint & Respawn
    CheckPoint & Respawn (Level1)

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -9,6 +9,10 @@ public class PlayerHealth : MonoBehaviour
     public int health;
     public int maxHealth = 10;
     public SpriteRenderer player;
+
+    public GameObject PlayerRs;
+    public Transform respawnPoint;
+
     public playerMovement movement;
 
     private Coroutine damageCoroutine;
@@ -20,9 +24,19 @@ public class PlayerHealth : MonoBehaviour
     public bool once = true;
 
 
+    private Vector3 checkpoint1Position;
+    private Vector3 checkpoint2Position;
+    private int lastCheckpoint = 0;
+
+
+
     void Start()
     {
         health = maxHealth;
+
+        checkpoint1Position = new Vector3(18, 8.25f, 0);
+        checkpoint2Position = new Vector3(56, 9, 0);
+
     }
 
     private void Update()
@@ -43,32 +57,59 @@ public class PlayerHealth : MonoBehaviour
     {
         health -= amount;
 
-        if(AudioManager.instance != null)
+        if (AudioManager.instance != null)
         {
             AudioManager.instance.PlayPlayerHitSound();
         }
 
         if (health <= 0)
         {
-            player.enabled = false;
-            movement.enabled = false;
-            Destroy(gameObject, 0.1f);
-            SceneManager.LoadScene("EndScene");
+            if (lastCheckpoint == 3)
+            {
+                player.enabled = false;
+                movement.enabled = false;
+                Destroy(gameObject, 0.1f);
+                SceneManager.LoadScene("EndScene");
+            }
+            else
+            {
+                Respawn();
+            }
+
+
 
         }
     }
 
     public void AddHealth()
     {
-        if(health < maxHealth)
+        if (health < maxHealth)
         {
             health += 1;
         }
-    
+
     }
+
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+
+        if (other.CompareTag("CheckPoint1"))
+        {
+            lastCheckpoint = 1;
+        }
+        else if (other.CompareTag("CheckPoint2"))
+        {
+            lastCheckpoint = 2;
+        }
+        else if (other.CompareTag("CheckPoint3"))
+        {
+            lastCheckpoint = 3;
+        }
+
+
+
         if (other.CompareTag("Light"))
         {
             isInLight = true;
@@ -91,6 +132,8 @@ public class PlayerHealth : MonoBehaviour
 
             Destroy(other.gameObject);
         }
+
+
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -108,7 +151,7 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    
+
 
     private IEnumerator DamageOverTime(float interval, int damageAmount)
     {
@@ -118,6 +161,24 @@ public class PlayerHealth : MonoBehaviour
             yield return new WaitForSeconds(interval);
         }
     }
+
+
+    private void Respawn()
+    {
+        health = maxHealth;
+        if (lastCheckpoint == 1)
+        {
+            PlayerRs.transform.position = checkpoint1Position;
+        }
+        else if (lastCheckpoint == 2)
+        {
+            PlayerRs.transform.position = checkpoint2Position;
+        }
+        player.enabled = true;
+        movement.enabled = true;
+    }
+
+
 
 
 }
